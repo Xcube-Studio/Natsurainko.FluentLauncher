b87870a4d35caa2c0782bc5c1dd5a8a576a7a16f
Jincheng You <jincheng.you@outlook.com>
2023-02-21 14:26:46 +0000

    Add exception dialog

--- Natsurainko.FluentLauncher/App.xaml.cs
+++ Natsurainko.FluentLauncher/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
 using Natsurainko.FluentLauncher.Components;
 using Natsurainko.FluentLauncher.Components.CrossProcess;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading;
 using Windows.Storage;
 using Windows.UI.Popups;
+using System.Windows.Input;
 
 namespace Natsurainko.FluentLauncher;
 
@@ -44,8 +46,10 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        
+        // Global exception handlers
         UnhandledException += App_UnhandledException;
-        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
     }
 
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
@@ -57,52 +61,84 @@ public partial class App : Application
     public static MainWindow MainWindow { get; private set; }
 
 
-    #region 全局错误收集
+    #region Global exception handlers
 
-    private void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+    private static void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
     {
-        App.MainWindow.DispatcherQueue?.TryEnqueue(async () =>
+        // 这里不能设置e.Handled，MS Learn文档表示只能用于记录日志后退出，或许可以尝试专门在新的进程上开一个窗口提示？
+        // https://learn.microsoft.com/en-us/dotnet/api/system.appdomain?view=net-7.
+
+        if (!e.IsTerminating && e.ExceptionObject is Exception ex)
         {
-            var stringBuilder = new StringBuilder()
-                .AppendLine(e.ExceptionObject.GetType().FullName)
-                .AppendLine(e.ExceptionObject.ToString());
-
-            var dialog = new MessageDialog(stringBuilder.ToString(), "程序遇到不可恢复的问题，请向开发者提供此窗口截图\r\n" +
-                "The program has encountered an unrecoverable problem, please provide the developer with a screenshot of this window");
-
-            dialog.Commands.Add(new UICommand("退出 Exit", (ui) => App.Current.Exit()));
-            await dialog.ShowAsync();
-        });
+            ProcessException(ex);
+        }
     }
 
-    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    private static void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
     {
         e.Handled = true;
-
-        var window = new Window();
-        window.Content = new Microsoft.UI.Xaml.Controls.TextBlock() { Text = "Hello" };
-        window.Activate();
-
-        //App.MainWindow.DispatcherQueue?.TryEnqueue(async () =>
-        //{
-        //    var stringBuilder = new StringBuilder()
-        //        .AppendLine(e.Exception.GetType().FullName)
-        //        .AppendLine(e.Exception.ToString());
-
-        //    if (e.Exception.InnerException != null)
-        //    {
-        //        stringBuilder.AppendLine("InnerException:");
-        //        stringBuilder.AppendLine(e.Exception.InnerException.ToString());
-        //    }
-
-        //    var dialog = new MessageDialog(stringBuilder.ToString(), "程序遇到不可恢复的问题，请向开发者提供此窗口截图\r\n" +
-        //        "The program has encountered an unrecoverable problem, please provide the developer with a screenshot of this window");
-
-        //    dialog.Commands.Add(new UICommand("退出 Exit", (ui) => App.Current.Exit()));
-        //    await dialog.ShowAsync();
-        //});
+        ProcessException(e.Exception);
     }
 
+    public static string GetErrorMessage(Exception e)
+    {
+        if (e is null) return string.Empty;
+
+        var stringBuilder = new StringBuilder()
+            .AppendLine(e.GetType().FullName)
+            .AppendLine(e.ToString());
+
+        if (e.InnerException != null)
+        {
+            stringBuilder.AppendLine("InnerException:");
+            stringBuilder.AppendLine(e.InnerException.ToString());
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    /// <summary>
+    /// Show error message in an appropriate way
+    /// </summary>
+    /// <param name="errorMessage"></param>
+    public static void ShowErrorMessage(string errorMessage)
+    {
+        errorMessage = errorMessage + errorMessage + errorMessage;
+
+        if (App.MainWindow is not null)
+        {
+            App.MainWindow.DispatcherQueue?.TryEnqueue(async () =>
+            {
+                var dialog = new ContentDialog
+                {
+                    XamlRoot = MainWindow.Content.XamlRoot,
+                    Title = "程序运行出现问题\nThe program has encountered a problem",
+                    Content = new ScrollViewer() 
+                    { 
+                        Content = new TextBlock { Text = errorMessage, IsTextSelectionEnabled = true },
+                        HorizontalScrollBarVisibility = ScrollBarVisibility.Auto
+                    },
+                    PrimaryButtonText = "关闭 Close"
+                };
+                await dialog.ShowAsync();
+            });
+        }
+        else
+        {
+            // Error in initializing MainWindow: display the exception in a deciated new window
+            var window = new Window();
+            window.Content = new TextBlock() { Text = errorMessage };
+            window.Activate();
+        }
+    }
+
+    public static void ProcessException(Exception ex)
+    {
+        var errorMessage = GetErrorMessage(ex);
+        // TODO: Log the error message
+
+        ShowErrorMessage(errorMessage);
+    }
 
     #endregion
 }

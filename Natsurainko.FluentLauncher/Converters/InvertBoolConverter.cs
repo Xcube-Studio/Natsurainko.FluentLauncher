8f094bbfd228159e0e374f8f586eaac0c5bbdfcb
Jincheng You <jincheng.you@outlook.com>
2023-03-13 13:55:48 +0000

    Update Java info ComboBox

---
+++ Natsurainko.FluentLauncher/Converters/InvertBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Natsurainko.FluentCore.Model.Install.Vanilla;
+using Natsurainko.FluentLauncher.Components.FluentCore;
+using System;
+using System.Collections.Generic;
+
+namespace Natsurainko.FluentLauncher.Converters;
+
+public class InvertBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not bool b)
+            throw new ArgumentException(nameof(value));
+
+        return !b;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not bool b)
+            throw new ArgumentException(nameof(value));
+
+        return !b;
+    }
+}

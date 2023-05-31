52ca32a627f32d0e11ee72edbf3221778a6c5510
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2023-05-31 23:00:27 +0100

    Update AppSettingsManagement

---
+++ Natsurainko.FluentLauncher/ViewModels/Common/SettingsViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using AppSettingsManagement.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natsurainko.FluentLauncher.ViewModels.Common;
+
+abstract class SettingsViewModelBase : ObservableObject
+{
+    ~SettingsViewModelBase()
+    {
+        if (this is ISettingsViewModel settingsViewModel)
+            settingsViewModel.RemoveSettingsChagnedHandlers();
+    }
+}

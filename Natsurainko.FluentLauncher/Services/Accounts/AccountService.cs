62b1352c7be8e6e6a4339f57ef56d88e7bca6301
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2023-06-10 16:33:58 +0100

    Define methods and events supported

--- Natsurainko.FluentLauncher/Services/Accounts/AccountService.cs
+++ Natsurainko.FluentLauncher/Services/Accounts/AccountService.cs
@@ -8,14 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Natsurainko.FluentCore.Model.Auth;
 using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace Natsurainko.FluentLauncher.Services.Accounts;
 
 class AccountService
 {
-    public ReadOnlyCollection<IAccount> Accounts { get; }
-    public IAccount ActiveAccount { get; private set; }
-
+    public ReadOnlyObservableCollection<IAccount> Accounts { get; }
     private readonly ObservableCollection<IAccount> _accounts = new();
 
     public event NotifyCollectionChangedEventHandler AccountsChanged
@@ -24,13 +23,21 @@ class AccountService
         remove => _accounts.CollectionChanged -= value;
     }
 
+    public IAccount ActiveAccount { get; private set; }
+
+    public event PropertyChangedEventHandler ActiveAccountChanged;
+
+
     public AccountService()
     {
-        
-        Accounts = _accounts.AsReadOnly();
+        Accounts = new ReadOnlyObservableCollection<IAccount>(_accounts);
     }
 
-
+    /// <summary>
+    /// Set an account as the active account
+    /// </summary>
+    /// <param name="account">The account to be activated</param>
+    /// <exception cref="ArgumentException">Thrown when the account provided is not managed by the AccountService</exception>
     public void Activate(IAccount account)
     {
         if (_accounts.Contains(account))
@@ -39,11 +46,22 @@ class AccountService
             throw new ArgumentException($"{account} is not an account managed by AccountService", nameof(account));
     }
 
+    /// <summary>
+    /// Remove an account managed by AccountService
+    /// </summary>
+    /// <param name="account">Account to be removed</param>
+    /// <returns>Returns true if successful</returns>
     public bool Remove(IAccount account)
     {
         return _accounts.Remove(account);
     }
 
+    /// <summary>
+    /// Add an account to the AccountService by authentication
+    /// </summary>
+    /// <param name="accountType">Type of the account to be added</param>
+    /// <param name="param">Authentication parameters</param>
+    /// <returns>Returns true if successful</returns>
     public bool AddAccount(AccountType accountType, object param)
     {
         throw new NotImplementedException();

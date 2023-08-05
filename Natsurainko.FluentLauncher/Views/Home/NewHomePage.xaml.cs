a7c6e217da0d6bcc4302ec7dd68c10df9422d774
natsurainko <a-275@qq.com>
2023-08-05 18:21:20 +0800

    NewHomePage Right Drawer

--- Natsurainko.FluentLauncher/Views/Home/NewHomePage.xaml.cs
+++ Natsurainko.FluentLauncher/Views/Home/NewHomePage.xaml.cs
@@ -10,4 +10,15 @@ public sealed partial class NewHomePage : Page
         this.InitializeComponent();
         DataContext = App.GetService<HomeViewModel>();
     }
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        splitView.IsPaneOpen = !splitView.IsPaneOpen;
+    }
+
+    private void Page_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        SharedShadow.Receivers.Add(BackgroundGrid);
+        PanelGrid.Translation += new System.Numerics.Vector3(0, 0, 48);
+    }
 }

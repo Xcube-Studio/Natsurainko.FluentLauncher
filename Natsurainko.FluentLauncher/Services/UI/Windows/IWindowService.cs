b3eaaae78b29c9fc8a7e96a635139feb40568c23
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2023-08-17 10:09:57 +0800

    Add more WindowService methods

--- Natsurainko.FluentLauncher/Services/UI/Windows/IWindowService.cs
+++ Natsurainko.FluentLauncher/Services/UI/Windows/IWindowService.cs
@@ -9,8 +9,18 @@ namespace Natsurainko.FluentLauncher.Services.UI.Windows;
 // Sevice for operations of a window
 interface IWindowService
 {
+    string Title { get; set; }
+
     /// <summary>
     /// Close the window
     /// </summary>
     void Close();
+    /// <summary>
+    /// Hide the window
+    /// </summary>
+    void Hide();
+    /// <summary>
+    /// Activate the window
+    /// </summary>
+    void Activate();
 }
--- Natsurainko.FluentLauncher/Services/UI/Windows/WindowService.cs
+++ Natsurainko.FluentLauncher/Services/UI/Windows/WindowService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinUIEx;
 
 namespace Natsurainko.FluentLauncher.Services.UI.Windows;
 
@@ -14,13 +15,18 @@ class WindowService : IWindowService
 {
     private readonly Window _window;
 
+    public string Title
+    {
+        get => _window.Title;
+        set => _window.Title = value;
+    }
+
     public WindowService(Window window)
     {
         _window = window;
     }
 
-    public void Close()
-    {
-        _window.Close();
-    }
+    public void Close() => _window.Close();
+    public void Hide() => _window.Hide();
+    public void Activate() => _window.Activate();
 }

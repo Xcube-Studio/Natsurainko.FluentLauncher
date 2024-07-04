12d1f17cc337849ece6612c9d3e5a1c41fe919fe
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-07-04 20:51:59 +0800

    Override Main()

---
+++ Natsurainko.FluentLauncher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Natsurainko.FluentLauncher;
+using System;
+using WinRT;
+
+namespace Natsurainko.FluentLauncher;
+
+public static class Program
+{
+    [DllImport("Microsoft.ui.xaml.dll")]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    private static extern void XamlCheckProcessRequirements();
+
+    [GeneratedCode("Microsoft.UI.Xaml.Markup.Compiler", " 3.0.0.2404")]
+    [DebuggerNonUserCode]
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        XamlCheckProcessRequirements();
+        ComWrappersSupport.InitializeComWrappers();
+        Application.Start(delegate
+        {
+            DispatcherQueueSynchronizationContext synchronizationContext = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(synchronizationContext);
+            new App();
+        });
+    }
+}

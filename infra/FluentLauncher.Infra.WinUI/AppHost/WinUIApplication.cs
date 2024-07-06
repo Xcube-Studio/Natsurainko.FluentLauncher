c91691bd4e88f5b49ff5298eacfae8be10af6371
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-07-06 10:35:59 +0800

    [Infra] Add generic host for WinUI (#240)
    * Override Main()
    
    * Use top-level statement
    
    * Add generic host
    
    * WinUI generic host prototype
    
    * Fix app lifetime and configure shutdown
    
    * Clean up
    
    * Add builder methods for HostApplicationBuilder
    
    * Enable generic host for service initialization
    
    * Clean up Program.cs

---
+++ infra/FluentLauncher.Infra.WinUI/AppHost/WinUIApplication.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WinRT;
+
+namespace FluentLauncher.Infra.WinUI.AppHost;
+
+public class WinUIApplication : IHost
+{
+    public IServiceProvider Services => Host.Services;
+
+    private readonly Func<Application> _createApplicationFunc;
+
+    public IHost Host { get; init; }
+
+    public static WinUIApplicationBuilder CreateBuilder(Func<Application> createApplicationFunc)
+    {
+        return new WinUIApplicationBuilder(createApplicationFunc);
+    }
+
+    internal WinUIApplication(Func<Application> createApplicationFunc, IHost host)
+    {
+        _createApplicationFunc = createApplicationFunc;
+        Host = host;
+    }
+
+    public void Dispose()
+    {
+        return;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken = default)
+    {
+        [DllImport("Microsoft.ui.xaml.dll")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+        static extern void XamlCheckProcessRequirements();
+
+        var hostAppLifetime = Services.GetRequiredService<IHostApplicationLifetime>();
+
+        // Task that models the execution of the Microsoft.UI.Xaml.Application.Start method
+        // Completes when the MUX Application exits.
+        var winUIStartedTcs = new TaskCompletionSource();
+
+        Task.Run(() =>
+        {
+            try
+            {
+                XamlCheckProcessRequirements();
+                ComWrappersSupport.InitializeComWrappers();
+                Application.Start(delegate
+                {
+                    try
+                    {
+                        DispatcherQueueSynchronizationContext synchronizationContext = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                        SynchronizationContext.SetSynchronizationContext(synchronizationContext);
+                        _createApplicationFunc();
+                        winUIStartedTcs.SetResult(); // Signal that WinUI has started successfully
+                    }
+                    catch (Exception ex)
+                    {
+                        winUIStartedTcs.SetException(ex); // Signal that an exception is thrown during initialization
+                    }
+                });
+                hostAppLifetime.StopApplication(); // WinUI app exits normally
+            }
+            catch (Exception ex)
+            {
+                winUIStartedTcs.SetException(ex); // Signal that an exception is thrown during initialization
+            }
+        }, cancellationToken);
+
+        return winUIStartedTcs.Task;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}

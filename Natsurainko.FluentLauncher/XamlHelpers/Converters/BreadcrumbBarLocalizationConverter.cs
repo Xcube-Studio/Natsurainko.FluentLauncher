45b1e51ff4bf39fd9a455fda3c0439b08e38bab3
natsurainko <a-275@qq.com>
2024-07-14 22:24:01 +0800

    本地化外观设置页面，更换部分新图标

---
+++ Natsurainko.FluentLauncher/XamlHelpers/Converters/BreadcrumbBarLocalizationConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Natsurainko.FluentLauncher.Utils;
+using System;
+
+#nullable disable
+namespace Natsurainko.FluentLauncher.XamlHelpers.Converters;
+
+public class BreadcrumbBarLocalizationConverter : IValueConverter
+{
+    public string BasePath { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not string text)
+            return string.Empty;
+
+        return ResourceUtils.GetValue(BasePath, $"_{text}");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}

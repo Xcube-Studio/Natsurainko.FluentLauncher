b6d63be20b435594824e2f63cb25ec4dc465ffe6
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-09-05 20:00:39 +0800

    Fix AddVmArgumentDialogViewModel

--- Natsurainko.FluentLauncher/ViewModels/Common/AddVmArgumentDialogViewModel.cs
+++ Natsurainko.FluentLauncher/ViewModels/Common/AddVmArgumentDialogViewModel.cs
@@ -3,18 +3,18 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
 using Natsurainko.FluentLauncher.Utils;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
-#nullable disable
 namespace Natsurainko.FluentLauncher.ViewModels.Common;
 
 internal partial class AddVmArgumentDialogViewModel : ObservableObject
 {
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(AddCommand))]
-    private string argument;
+    private string? argument;
 
     private readonly Action<string> _addAction;
-    private ContentDialog _dialog;
+    private ContentDialog _dialog = null!;
 
     public AddVmArgumentDialogViewModel(Action<string> addAction)
     {
@@ -25,18 +25,19 @@ internal partial class AddVmArgumentDialogViewModel : ObservableObject
     public void LoadEvent(object args)
     {
         var grid = args.As<Grid, object>().sender;
-        _dialog = grid.FindName("Dialog") as ContentDialog;
+        _dialog = (ContentDialog)grid.FindName("Dialog");
     }
 
     [RelayCommand(CanExecute = nameof(EnableAddArgument))]
     public void Add()
     {
-        _addAction(Argument);
+        _addAction(Argument!); // Argument is not null when EnableAddArgument is true
         _dialog.Hide();
     }
 
     [RelayCommand]
     public void Cancel() => _dialog.Hide();
 
+    [MemberNotNullWhen(true, nameof(Argument))]
     private bool EnableAddArgument => !string.IsNullOrEmpty(Argument);
 }

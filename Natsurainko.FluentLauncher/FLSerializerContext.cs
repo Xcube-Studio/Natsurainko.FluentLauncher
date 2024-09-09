1f5562239df8fc6b2c3b079964fdc7733f31da24
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-09-09 16:24:46 +0800

    Add global serializer context for FluentLauncher

---
+++ Natsurainko.FluentLauncher/FLSerializerContext.cs
@@ -0,0 +1,23 @@
+﻿using Nrk.FluentCore.GameManagement.Installer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Natsurainko.FluentLauncher;
+
+// Settings
+[JsonSerializable(typeof(int))]
+[JsonSerializable(typeof(uint))]
+[JsonSerializable(typeof(bool))]
+[JsonSerializable(typeof(float))]
+[JsonSerializable(typeof(double))]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(string[]))]
+[JsonSerializable(typeof(Windows.UI.Color))]
+[JsonSerializable(typeof(WinUIEx.WindowState))]
+internal partial class FLSerializerContext : JsonSerializerContext
+{
+}

5f09ce345848e677e9d265c70165d0e9cb006f17
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-12-16 09:51:50 +0800

    Note for crash when navigating back in a wizard

--- Natsurainko.FluentLauncher/Views/AuthenticationWizard/ChooseAccountTypePage.xaml.cs
+++ Natsurainko.FluentLauncher/Views/AuthenticationWizard/ChooseAccountTypePage.xaml.cs
@@ -6,6 +6,11 @@ namespace Natsurainko.FluentLauncher.Views.AuthenticationWizard;
 public sealed partial class ChooseAccountTypePage : Page
 {
     // TODO: Fix crash due to DataContext being null when navigating back in the wizard
+
+    // Investigation: Two instances of this page are referencing the same VM after navigating back.
+    // Changes on the page being displayed cause changes in the VM, which triggers an update on the page unloaded.
+    // Radio buttons on the page unloaded are updated, but its DataContext is null, resulting in a crash.
+
     ChooseAccountTypeViewModel VM => (ChooseAccountTypeViewModel)DataContext;
 
     public ChooseAccountTypePage()

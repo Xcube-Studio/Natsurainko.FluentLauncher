c095555a6e12d5045caa2afb72b699cefbbeefcb
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-12-19 11:40:32 +0800

    Add FluentLauncher.Infra.UI.Dialogs

---
+++ infra/FluentLauncher.Infra.UI/Dialogs/IDialogProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FluentLauncher.Infra.UI.Dialogs;
+
+public interface IDialogProvider
+{
+    IReadOnlyDictionary<string, DialogDescriptor> RegisteredDialogs { get; }
+
+    object GetDialog(string key);
+
+    object? GetViewModel(string key);
+}
+
+public record DialogDescriptor
+{
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
+    public Type DialogType { get; init; }
+
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
+    public Type? ViewModelType { get; init; }
+
+    public DialogDescriptor(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type dialogType,
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type? vmType)
+    {
+        DialogType = dialogType;
+        ViewModelType = vmType;
+    }
+}

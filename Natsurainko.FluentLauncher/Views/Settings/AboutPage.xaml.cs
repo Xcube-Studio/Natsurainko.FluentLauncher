53e75f42f627131ca8ce81a39c7d161bc447a96c
natsurainko <a-275@qq.com>
2025-01-05 18:24:17 +0800

    在 Dev 通道下禁用更新检查

--- Natsurainko.FluentLauncher/Views/Settings/AboutPage.xaml.cs
+++ Natsurainko.FluentLauncher/Views/Settings/AboutPage.xaml.cs
@@ -11,7 +11,9 @@ public sealed partial class AboutPage : Page, IBreadcrumbBarAware
     {
         InitializeComponent();
 
-#if FLUENT_LAUNCHER_PREVIEW_CHANNEL
+#if FLUENT_LAUNCHER_DEV_CHANNEL
+        this.AboutCard.IsEnabled = false;
+#elif FLUENT_LAUNCHER_PREVIEW_CHANNEL
         this.Loaded += (object sender, Microsoft.UI.Xaml.RoutedEventArgs e) =>
         {
             this.CheckUpdateText.Text = "Check Update From Preview Channel";

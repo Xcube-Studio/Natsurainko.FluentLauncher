3c4199820e617dccb1b1e75c46223ab9278c2c06
natsurainko <a-275@qq.com>
2025-01-08 14:24:19 +0800

    修复 using

--- Natsurainko.FluentLauncher/Services/Network/UpdateService.cs
+++ Natsurainko.FluentLauncher/Services/Network/UpdateService.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
--- Natsurainko.FluentLauncher/Views/Common/AuthenticationWizardDialog.xaml.cs
+++ Natsurainko.FluentLauncher/Views/Common/AuthenticationWizardDialog.xaml.cs
@@ -1,9 +1,6 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Natsurainko.FluentLauncher.Services.Accounts;
 using Natsurainko.FluentLauncher.Services.Settings;
-using Natsurainko.FluentLauncher.Services.UI;
-using Natsurainko.FluentLauncher.ViewModels.Common;
 
 namespace Natsurainko.FluentLauncher.Views.Common;
 

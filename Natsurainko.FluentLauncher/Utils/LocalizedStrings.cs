0f10a12a62178474a08d1983a00e7d5e4e73f8a7
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2025-01-20 19:47:57 +0000

    Generate .resw string accessors in LocalizedStrings

---
+++ Natsurainko.FluentLauncher/Utils/LocalizedStrings.cs
@@ -0,0 +1,13 @@
+﻿using FluentLauncher.Infra.LocalizedStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natsurainko.FluentLauncher.Utils;
+
+[GeneratedLocalizedStrings]
+partial class LocalizedStrings
+{
+}

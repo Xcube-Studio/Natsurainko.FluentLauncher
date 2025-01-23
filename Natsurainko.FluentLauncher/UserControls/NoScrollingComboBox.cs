0f71c251483e9d2a911ea8bd8caf99391a6aaa81
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2025-01-23 14:44:06 +0000

    Disable changing language by scrolling on the ComboBox

---
+++ Natsurainko.FluentLauncher/UserControls/NoScrollingComboBox.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natsurainko.FluentLauncher.UserControls;
+
+class NoScrollingComboBox : ComboBox
+{
+    protected override void OnPointerWheelChanged(PointerRoutedEventArgs e)
+    {
+        // Disable changing option by scrolling
+    }
+}

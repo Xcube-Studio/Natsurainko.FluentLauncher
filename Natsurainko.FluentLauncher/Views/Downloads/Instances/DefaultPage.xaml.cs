73007d6ef48ce46ea51565c08b96dbb0ba3cb8bf
natsurainko <a-275@qq.com>
2025-04-07 16:47:35 +0800

    调整 UI 布局自适应

--- Natsurainko.FluentLauncher/Views/Downloads/Instances/DefaultPage.xaml.cs
+++ Natsurainko.FluentLauncher/Views/Downloads/Instances/DefaultPage.xaml.cs
@@ -1,4 +1,5 @@
 using FluentLauncher.Infra.UI.Navigation;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Natsurainko.FluentLauncher.ViewModels.Downloads.Instances;
 using System;
@@ -17,5 +18,36 @@ public sealed partial class DefaultPage : Page, IBreadcrumbBarAware
         this.InitializeComponent(); 
     }
 
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        Update();
+        SizeChanged += (_, _) => Update();
+    }
+
     internal static string FormatDate(string date) => DateTime.Parse(date).ToString(CultureInfo.CurrentCulture.DateTimeFormat);
+
+    void Update()
+    {
+        if (CD1 == null)
+            return;
+
+        if (App.MainWindow.Width <= 641)
+        {
+            CD2.Width = GridLength.Auto;
+            CD1.Width = new GridLength(0, GridUnitType.Pixel);
+            RD1.Height = new GridLength(4, GridUnitType.Pixel);
+
+            Grid.SetRow(LatestSnapshotPanel, 2);
+            Grid.SetColumn(LatestSnapshotPanel, 0);
+        }
+        else
+        {
+            CD2.Width = new GridLength(1, GridUnitType.Star);
+            CD1.Width = new GridLength(4, GridUnitType.Pixel);
+            RD1.Height = new GridLength(0, GridUnitType.Pixel);
+
+            Grid.SetRow(LatestSnapshotPanel, 0);
+            Grid.SetColumn(LatestSnapshotPanel, 2);
+        }
+    }
 }
